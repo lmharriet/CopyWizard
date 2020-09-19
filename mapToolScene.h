1583b8739194fabf5be6b8403c43a3dddc9d3eea
혜림 이 <lharriet27@gmail.com>
2020-09-20 00:06:13 +0900

    다시만들기

--- mapToolScene.h
+++ mapToolScene.h
@@ -5,45 +5,10 @@
 class mapToolScene : public gameNode
 {
 private:
-	///TILE///
-	tagMap tile[MAXTILE]; //기본 맵 타일
 
-	//SINGLE//
-	tagSample sTile[9]; //선택 맵 타일 <단일>
-	//  SET //
-	tagSample sTile_set;//선택 맵 타일 <세트>
-	///BUTTON///
-	RECT dragChangeButton;
-
-	deque<int> backup;
-
-	tagMouse mouse;
-	STATE state;
-
-	//button down
-	bool isLeftDown;
-	bool isLeftUp;
-	bool isLeftkey;
-
-	//cam
-	RECT camRC;
-	float camX, camY;
 public:
 	HRESULT init();
 	void release();
 	void update();
 	void render();
-
-	void save();
-	void load();
-	void dragTile();
-	void singleTile();
-
-	bool checkPt(POINT pt, POINT _pt);
-
-	//text view
-	void textRender();
-
-	//cam
-	RECT moveRC(int x, int y, int width, int height) { return RectMake(x + camX, y + camY, width, height); }
 };
