d9baa484236926b17db35c3456a24c5171e6d514
ricethief0 <net050722@gmail.com>
2020-09-29 22:37:12 +0900

    monster(golem) render

--- gameScene.cpp
+++ gameScene.cpp
@@ -34,6 +34,7 @@ void gameScene::release()
 void gameScene::update()
 {
 	_player->update();
+	_golem->update();
 
 	cam = RectMakeCenter(_player->getX(), _player->getY(), WINSIZEX, WINSIZEY);
 	//checkArea = RectMakeCenter(_player->getX(), _player->getY(), 400, 400);
@@ -196,7 +197,7 @@ void gameScene::render()
 
 	viewText();
 
-	//_golem->render(); //테스트중 (몬스터골렘)
+	_golem->render(); //테스트중 (몬스터골렘)
 }
 
 void gameScene::loadMap(const char* mapFileName)
--- golem.cpp
+++ golem.cpp
@@ -4,6 +4,12 @@
 HRESULT golem::init()
 {
     img = IMAGEMANAGER->addFrameImage("golem", "wizard/Golem.bmp", 720, 700, 6, 5);
+    cul = { 0,0 };
+    pos.x = 849;
+    pos.y = 360;
+    speed = 5.f;
+    isFindWayOn = false; //길찾기 온/오프
+
     return S_OK;
 }
 
@@ -13,9 +19,26 @@ void golem::release()
 
 void golem::update()
 {
+    cul.x = CAMERAMANAGER->GetRelativeX(pos.x);
+    cul.y = CAMERAMANAGER->GetRelativeY(pos.y);
+    
+    //if (!isFindWayOn) //길찾기 오프
+    //{
+    //  angle = getAngle(cul.x, cul.y, 853, 360);
+    //  if(pos.x !=849)    pos.x += cos(angle) * speed;
+    //  if(pos.y !=360)    pos.y += -sin(angle) * speed;
+    //}
+    //else { //길찾기 온
+    //
+    //    
+    //}
+
+
 }
 
 void golem::render()
 {
-    img->frameRender(getMemDC(), WINSIZEX / 2, WINSIZEY / 2);
+    rc = RectMake(cul.x, cul.y, img->getFrameWidth(), img->getFrameHeight());
+    FrameRect(getMemDC(), rc, RGB(0, 0, 0));
+    img->frameRender(getMemDC(), cul.x, cul.y);
 }
