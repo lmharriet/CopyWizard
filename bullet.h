2010fc8cfae0e1c6b1dc8dcb2dc9f850724f4d61
ricethief0 <net050722@gmail.com>
2020-10-08 17:01:17 +0900

    bullet image defalut

--- bullet.h
+++ bullet.h
@@ -66,7 +66,7 @@ public:
 	void render();
 	
 	//총알발사
-	void fire(float x, float y, float angle, float speed, int damage, bool image);
+	void fire(float x, float y, float angle, float speed, int damage, bool image = false);
 	//총알무브
 	void move();
 	//총알 비어있는지 확인
