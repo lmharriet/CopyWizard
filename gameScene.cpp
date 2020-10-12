6bca68dca5d12ffaa22babd73c6b87157ae6e1e4
suhyun <tngus10778>
2020-10-12 14:29:22 +0900

    gameScene fix

--- gameScene.cpp
+++ gameScene.cpp
@@ -141,27 +141,7 @@ void gameScene::render()
 		CAMERAMANAGER->FrameRender(getMemDC(), img, tile[num].rc.left, tile[num].rc.top, tile[num].frame.x, tile[num].frame.y);
 	}
 
-	//CAMERAMANAGER->Rectangle(getMemDC(), checkArea);
-	//Rectangle(getMemDC(), checkArea);
-	//_player->render();
-
-	DROP->render(getMemDC());
-	EFFECT->pRender(getMemDC());
-
-	bool enemyProRender = false;
-	for (int i = 0; i < enemy->getMinion().size(); i++)
-	{
-		if (_player->getY() >= enemy->getMinion()[i]->getCenterY())
-		{
-			enemy->getMinion()[i]->render();
-		}
-	}
-
-	bool isRender = false;
-
-	//CAMERAMANAGER->Rectangle(getMemDC(), cam);
-
-	//object image render
+	//deco image render
 	for (int f = 0; f < vTile.size(); f++)
 	{
 		int i = vTile[f];
@@ -172,6 +152,28 @@ void gameScene::render()
 
 		CAMERAMANAGER->Render(getMemDC(), img, tile[i].rc.left, tile[i].rc.top);
 	}
+
+	//CAMERAMANAGER->Rectangle(getMemDC(), checkArea);
+	//Rectangle(getMemDC(), checkArea);
+	//_player->render();
+
+	DROP->render(getMemDC());
+	EFFECT->pRender(getMemDC());
+
+	bool* eRender = new bool[enemy->getMinion().size()];
+
+	for (int i = 0; i < enemy->getMinion().size(); i++)
+	{
+		if (_player->getY() >= enemy->getMinion()[i]->getCenterY())
+		{
+			enemy->getMinion()[i]->render();
+			eRender[i] = true;
+		}
+	}
+
+	bool isRender = false;
+
+	//CAMERAMANAGER->Rectangle(getMemDC(), cam);
 
 	string key;
 	image* img;
@@ -470,12 +472,12 @@ void gameScene::render()
 	enemy->render();
 
 	if (!isRender) _player->render();
+
 	for (int i = 0; i < enemy->getMinion().size(); i++)
 	{
-		if (_player->getY() < enemy->getMinion()[i]->getCenterY())
-		{
-			enemy->getMinion()[i]->render();
-		}
+		if (eRender[i] == true)continue;
+		
+		enemy->getMinion()[i]->render();
 	}
 
 	//PARTICLE->render(getMemDC(), CAMERAMANAGER->getRect());
